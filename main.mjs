2f80a65cec48d8bba1d0f411048431eb714062e3
ale vel <ale.vrs@outlook.com>
2024-02-16 18:17:56 -0500

    Updates

--- main.mjs
+++ main.mjs
@@ -1,3 +1,12 @@
 import { showTrending } from "./utils/show-trending.mjs";
+import {
+  btnShow,
+  btnHide,
+  showContent,
+  hideContent,
+} from "./utils/show-hide-details.mjs";
 
 showTrending();
+
+btnShow.addEventListener("click", showContent);
+btnHide.addEventListener("click", hideContent);
