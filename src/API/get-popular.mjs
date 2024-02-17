bc4f5e3e3b5a45142176fc4868578c22c6f88ac5
ale vel <ale.vrs@outlook.com>
2024-02-16 23:33:52 -0500

    Create HTML elements from JS

---
+++ src/API/get-popular.mjs
@@ -0,0 +1,18 @@
+import { autorization } from "./authorization.mjs";
+
+const getPopular = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=33",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: autorization,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+export { getPopular };
---
+++ src/utils/show-popular.mjs
@@ -0,0 +1,63 @@
+import { getPopular } from "../API/get-popular.mjs";
+import { main } from "../nodes.mjs";
+
+const showPopular = async () => {
+  const data = await getPopular();
+  console.log(data);
+
+  data.results.map(movie => {
+    console.log(movie.title);
+
+    const section =
+      document.createElement("section");
+
+    const divDetails =
+      document.createElement("div");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+
+    const quality = document.createElement("p");
+
+    const divMore = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const rating = document.createElement("p");
+    const span = document.createElement("span");
+    const genres = document.createElement("p");
+    const spanGenres =
+      document.createElement("span");
+
+    const btnShow =
+      document.createElement("button");
+
+    const description =
+      document.createElement("section");
+    const cast = document.createElement("p");
+    const spanCast = document.createElement("span");
+    const overview = document.createElement("p");
+    const btnHide =
+      document.createElement("button");
+
+    // Clases
+    section.classList.add("main__section");
+    divDetails.classList.add("details-div");
+    figure.classList.add("details__figure");
+
+    quality.classList.add("details__quality");
+
+    divMore.classList.add("details__more");
+
+    btnShow.classList.add(
+      "button-description btn-show"
+    );
+    description.classList.add(
+      "description disabled"
+    );
+    cast.classList.add("description__cast");
+    overview.classList.add("description__overview");
+    btnHide.classList.add(
+      "button-description hide-btn"
+    );
+  });
+};
+
+export { showPopular };
