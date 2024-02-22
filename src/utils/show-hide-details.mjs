13aa04325271e1117fc4ac14b2615bf1e218a69b
Ale Roses <ale.vrs01@outlook.com>
2024-02-21 19:43:04 -0500

    Update styles

--- src/utils/show-hide-details.mjs
+++ src/utils/show-hide-details.mjs
@@ -1,17 +1,10 @@
-/* const show = document.querySelector(".btn-show");
-const hide = document.querySelector(".hide-btn"); */
-
-import { main } from "../nodes.mjs";
-
 const showContent = (event) => {
   console.log(event.currentTarget);
   const btnShowCont = event.currentTarget;
   console.log("Parent: ", btnShowCont.parentElement);
   const cardParent = btnShowCont.parentElement;
+  cardParent.classList.add("shadow");
 
-  /* console.log(cardParent.closest(".main__section"));
-  const mainSection = cardParent.closest(".main__section");
-  mainSection.style.display = "grid"; */
   cardParent.style.display = "grid";
 
   console.log("Selector: ", cardParent.querySelector(".description"));
@@ -25,19 +18,6 @@ const showContent = (event) => {
 };
 
 const hideContent = (event) => {
-  /* const btnShowCont = document.querySelector(".btn-show");
-  btnShowCont.style.display = "block";
-
-  const sectionCont = document.querySelector(".main__section");
-  sectionCont.style.display = "block";
-
-  const content = document.querySelector(".description");
-  content.style.display = "none";
-
-  const btnHideCont = document.querySelector(".hide-btn");
-  btnHideCont.style.display = "none"; */
-
-  /* const btnShowCont = event.querySelector(""); */
   console.log(event.currentTarget);
   const btnHideCont = event.currentTarget;
   console.log(btnHideCont.parentElement);
@@ -47,25 +27,10 @@ const hideContent = (event) => {
   console.log("main section: ", cardParent.closest(".main__section"));
   const mainSection = cardParent.closest(".main__section");
   /* mainSection.style.display = "block"; */
+  mainSection.classList.remove("shadow");
 
   const btnShowCont = mainSection.querySelector(".btn-show");
   btnShowCont.style.display = "block";
-
-  /* const btnShowCont =  */
-
-  /* const btnHideCont = event.currentTarget;
-  const cardParent = btnHideCont.parentElement;
-
-  const content = cardParent.querySelector(".description");
-  content.classList.add("disabled");
-
-  const mainSection = cardParent.closest(".main__section");
-  mainSection.style.display = "block";
-
-  const btnShowCont = cardParent.querySelector(".btn-show");
-  btnShowCont.style.display = "block";
-
-  btnHideCont.style.display = "none"; */
 };
 
 export { /* show, hide, */ showContent, hideContent };
