21e0d65a13b405e1ff38e0e52218503024aade57
Ale Roses <ale.vrs01@outlook.com>
2024-02-26 02:23:42 -0500

    Updates

--- utils/show-search-movie.mjs
+++ utils/show-search-movie.mjs
@@ -1,6 +1,16 @@
 import { getSearchMovies } from "../API/get-search-movie.mjs";
 
+const fatherSearch = document.querySelector(
+  ".search.disabled"
+);
+const searchIcon = document.querySelector(
+  ".section__icon--search"
+);
 const input = document.querySelector(".input");
+const totalMovies = document.querySelector(".total-movies");
+const sectionSearchMovie = document.querySelector(
+  ".section__search--movie"
+);
 const disabled = document.querySelector(
   ".section__show--movies.disabled"
 );
@@ -8,14 +18,26 @@ const containerMovies = document.querySelector(
   ".container-movies"
 );
 
+const sectionSearch = document.querySelector(
+  ".section__search.disabled"
+);
+const closeIcon = document.querySelector(".close-icon");
+
+let counter = 0;
+
 const showSearchMovie = async (name) => {
   const data = await getSearchMovies(name);
 
   containerMovies.innerHTML = "";
 
-  data.results.slice(0, 3).map((movie) => {
-    console.log(movie.release_date.split("-")[0]);
+  data.results.map((movies) => {
+    counter++;
 
+    totalMovies.innerHTML = `MOVIES (${counter}) - VIEW ALL`;
+    /* console.log(counter, movies.title); */
+  });
+
+  data.results.slice(0, 3).map((movie) => {
     const date = movie.release_date.split("-")[0];
     const vote = movie.vote_average.toFixed(1);
 
@@ -54,13 +76,25 @@ const showSearchMovie = async (name) => {
 input.addEventListener("input", () => {
   const filmName = input.value.toLowerCase();
 
-  if (filmName.length > 0) {
+  if (filmName.length > 1) {
     disabled.style.display = "grid";
 
     showSearchMovie(filmName);
   } else {
     disabled.style.display = "none";
   }
+});
+
+searchIcon.addEventListener("click", () => {
+  sectionSearch.classList.remove("disabled");
+  searchIcon.style.display = "none";
+  fatherSearch.classList.remove("disabled");
+});
+
+closeIcon.addEventListener("click", () => {
+  sectionSearch.classList.add("disabled");
+  searchIcon.style.display = "block";
+  fatherSearch.classList.add("disabled");
 });
 
 export { showSearchMovie };
