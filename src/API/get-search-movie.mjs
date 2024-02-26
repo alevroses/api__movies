a1dadf2abd6e646c651582b95a9d7d0c93a51164
Ale Roses <ale.vrs01@outlook.com>
2024-02-26 00:51:10 -0500

    Load found movies

---
+++ src/API/get-search-movie.mjs
@@ -0,0 +1,11 @@
+import { api_key } from "./authorization.mjs";
+
+const getSearchMovies = async (name) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${api_key}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+export { getSearchMovies };
--- src/main.mjs
+++ src/main.mjs
@@ -7,11 +7,13 @@ import { showTrending } from "./utils/show-trending.mjs";
 } from "./utils/show-hide-details.mjs"; */
 import { showPopular } from "./utils/show-popular.mjs";
 import { generatePageNumbers } from "./utils/create-page-numbers.mjs";
+import { showSearchMovie } from "./utils/show-search-movie.mjs";
 
 showTrending();
 showPopular();
 
 generatePageNumbers();
+/* showSearchMovie(); */
 
 /* show.addEventListener("click", showContent);
 hide.addEventListener("click", hideContent); */
