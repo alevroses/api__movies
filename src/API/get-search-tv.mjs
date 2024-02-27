5d64bb78adaad38537179c1f4acac10e0f682810
Ale Roses <ale.vrs01@outlook.com>
2024-02-26 20:09:47 -0500

    Show tv programs

---
+++ src/API/get-search-tv.mjs
@@ -0,0 +1,12 @@
+import { api_key } from "./authorization.mjs";
+
+const getSearchTv = async (name) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/tv?query=${name}&api_key=${api_key}`
+  );
+  const data = await response.json();
+  /* console.log(data); */
+  return data;
+};
+
+export { getSearchTv };
--- src/main.mjs
+++ src/main.mjs
@@ -1,19 +1,15 @@
 import { showTrending } from "./utils/show-trending.mjs";
-/* import {
-  show,
-  hide,
-  showContent,
-  hideContent,
-} from "./utils/show-hide-details.mjs"; */
 import { showPopular } from "./utils/show-popular.mjs";
 import { generatePageNumbers } from "./utils/create-page-numbers.mjs";
+
 import { showSearchMovie } from "./utils/show-search-movie.mjs";
+import { getSearchTv } from "./API/get-search-tv.mjs";
+import { showSearchTv } from "./utils/show-search-tv.mjs";
 
 showTrending();
 showPopular();
 
 generatePageNumbers();
-/* showSearchMovie(); */
 
 /* show.addEventListener("click", showContent);
 hide.addEventListener("click", hideContent); */
