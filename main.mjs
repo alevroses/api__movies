0ca6fd7a6a02bcceac4527482dbb14d4b6c733f5
Ale Roses <ale.vrs01@outlook.com>
2024-02-27 18:38:55 -0500

    Updates

--- main.mjs
+++ main.mjs
@@ -5,11 +5,13 @@ import { generatePageNumbers } from "./utils/create-page-numbers.mjs";
 import { showSearchMovie } from "./utils/show-search-movie.mjs";
 import { getSearchTv } from "./API/get-search-tv.mjs";
 import { showSearchTv } from "./utils/show-search-tv.mjs";
+import { showHidenLogin } from "./utils/show-hide-login.mjs";
 
 showTrending();
 showPopular();
 
 generatePageNumbers();
+showHidenLogin();
 
 /* show.addEventListener("click", showContent);
 hide.addEventListener("click", hideContent); */
