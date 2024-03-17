7f5854cc35d99c7cc87bcc89f13334ef5254ced4
Ale Roses <ale.vrs01@outlook.com>
2024-03-16 19:29:33 -0500

    Updates

--- utils/change-login.mjs
+++ utils/change-login.mjs
@@ -1,14 +1,17 @@
 const login = document.querySelector(".section__icons.login");
 
 const changeLogin = () => {
-  const loginBtn = document.createElement("button");
-  const signupBtn = document.createElement("button");
+  const loginBtn = document.createElement("a");
+  const signupBtn = document.createElement("a");
   loginBtn.textContent = "LOGIN";
   signupBtn.textContent = "SIGNUP";
 
   if (window.innerWidth > 739) {
     /* login.innerHTML = loginBtn.outerHTML; */
     login.innerHTML = "";
+    loginBtn.setAttribute("href", "./navigation/login.html");
+    /* loginBtn.href = "./navigation/login.html"; */
+
     login.append(loginBtn, signupBtn);
   }
 };
