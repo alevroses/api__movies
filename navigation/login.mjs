4220d6a53b94ae6804a77fb151ae6d2106dd07f0
Ale Roses <ale.vrs01@outlook.com>
2024-03-19 20:32:38 -0500

    Updates

--- navigation/login.mjs
+++ navigation/login.mjs
@@ -1,3 +1,5 @@
+import { top } from "../components/back-to-top.mjs";
+import { showFooter } from "../components/footer.mjs";
 import { changeLogin } from "../utils/change-login.mjs";
 import { showHidenLogin } from "../utils/show-hide-login.mjs";
 import { showMoviesGenres } from "../utils/show-movies-genres.mjs";
@@ -9,3 +11,9 @@ import { showSearchTv } from "../utils/show-search-tv.mjs";
 showHidenLogin();
 showMoviesGenres();
 changeLogin();
+
+showFooter();
+top();
+
+/* const x = document.querySelector(".footer-figure");
+console.log(x); */
