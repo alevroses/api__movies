36ada3d134ae1d3f44b26cfa91742ecb10c67abb
Ale Roses <ale.vrs01@outlook.com>
2024-03-20 10:04:22 -0500

    Create a tv program page

---
+++ src/navigation/shows.mjs
@@ -0,0 +1,8 @@
+import { changeLogin } from "../utils/change-login.mjs";
+import { showHidenLogin } from "../utils/show-hide-login.mjs";
+import { showMoviesGenres } from "../utils/show-movies-genres.mjs";
+
+showHidenLogin();
+showMoviesGenres();
+
+changeLogin();
